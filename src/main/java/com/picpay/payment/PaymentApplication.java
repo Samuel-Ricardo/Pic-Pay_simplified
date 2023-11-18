7a2ace54806df30118044a1b572efccc59cbb317
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-11-18 16:50:26 -0300

    [:card_file_box:] | setup: database - postgres (Docker)

--- src/main/java/com/picpay/payment/PaymentApplication.java
+++ src/main/java/com/picpay/payment/PaymentApplication.java
@@ -2,12 +2,22 @@ package com.picpay.payment;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class PaymentApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PaymentApplication.class, args);
+	}
+
+	@GetMapping
+	public ResponseEntity<String> index() {
+		return ResponseEntity.status(HttpStatus.OK).body("{message: \" Hello World! :D \"}");
 	}
 
 }
