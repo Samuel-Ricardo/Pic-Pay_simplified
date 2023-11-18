77b88adc70a4dfeec2820ccfb58d3c3c1b8664ee
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-11-18 16:41:33 -0300

    [:gear:] | create: config - properties > database (test/resource)

--- src/test/java/com/picpay/payment/PaymentApplicationTests.java
+++ src/test/java/com/picpay/payment/PaymentApplicationTests.java
@@ -2,7 +2,9 @@ package com.picpay.payment;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
+@ActiveProfiles("test")
 @SpringBootTest
 class PaymentApplicationTests {
 
