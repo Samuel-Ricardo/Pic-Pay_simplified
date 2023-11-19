159ed14d60a369f1e2426cb655d2630153ca2c88
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-11-19 13:38:25 -0300

    [:sparkles:] | update: entity - user > id (domain)

--- src/main/java/com/picpay/payment/domain/entities/user/User.java
+++ src/main/java/com/picpay/payment/domain/entities/user/User.java
@@ -6,6 +6,7 @@ import lombok.*;
 import org.springframework.beans.BeanUtils;
 
 import java.math.BigDecimal;
+import java.util.UUID;
 
 @Entity(name = "users") @Table(name = "users")
 @Getter @Setter
@@ -14,8 +15,8 @@ import java.math.BigDecimal;
 public class User {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
     private String firstName;
     private String lastName;
     @Column(unique = true)
