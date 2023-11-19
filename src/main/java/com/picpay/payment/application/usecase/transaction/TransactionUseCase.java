8abf421d4d3b3070ad310666f8cc92e328c77ea9
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-11-19 18:00:53 -0300

    [:sparkles:] | create: use-case - transaction (application)

---
+++ src/main/java/com/picpay/payment/application/usecase/transaction/TransactionUseCase.java
@@ -0,0 +1,25 @@
+package com.picpay.payment.application.usecase.transaction;
+
+import com.picpay.payment.domain.entities.transaction.Transaction;
+
+public class TransactionUseCase {
+    Transaction execute(Transaction transaction){
+        var sender = transaction.getSender();
+        var receiver = transaction.getReceiver();
+
+        sender
+            .setBalance(
+                sender
+                    .getBalance()
+                    .subtract(transaction.getAmount())
+            );
+
+        receiver
+            .setBalance(
+                receiver
+                    .getBalance()
+                    .add(transaction.getAmount()));
+
+        return transaction;
+    }
+}
