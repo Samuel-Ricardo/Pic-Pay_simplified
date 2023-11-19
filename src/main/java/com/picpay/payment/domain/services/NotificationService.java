518901a8c8ddf42b83982cb82b3b3199d1ec8bd2
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-11-19 16:47:49 -0300

    [:sparkles:] | create: service - notification (domain)

---
+++ src/main/java/com/picpay/payment/domain/services/NotificationService.java
@@ -0,0 +1,7 @@
+package com.picpay.payment.domain.services;
+
+import com.picpay.payment.domain.entities.user.User;
+
+public interface NotificationService {
+    void sendNotification(User user, String message);
+}
