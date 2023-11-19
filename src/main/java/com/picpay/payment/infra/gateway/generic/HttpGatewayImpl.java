fc588b481a36e8ef11f2fb95512cfd129f7ca1e8
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-11-19 15:12:42 -0300

    [:door:] | implements: gateway - generic > http (infra)

---
+++ src/main/java/com/picpay/payment/infra/gateway/generic/HttpGatewayImpl.java
@@ -0,0 +1,43 @@
+package com.picpay.payment.infra.gateway.generic;
+
+import com.picpay.payment.domain.gateways.generic.HttpGateway;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Primary;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.Map;
+
+@Primary
+@Component
+public class HttpGatewayImpl implements HttpGateway {
+
+    @Autowired
+    protected RestTemplate client;
+
+    @Override
+    public ResponseEntity<Map> get(String URL)  {
+        return client.getForEntity(URL, Map.class);
+    }
+
+    @Override
+    public ResponseEntity<Map> post(String URL, Object data) {
+        return null;
+    }
+
+    @Override
+    public ResponseEntity<Map> put(String URL, Object data) {
+        return  null;
+    }
+
+    @Override
+    public ResponseEntity<Map> patch(String URL, Object data) {
+        return null;
+    }
+
+    @Override
+    public ResponseEntity<Map> delete(String URL) {
+        return null;
+    }
+}
