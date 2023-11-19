2a24d52160c4ebace3269df69c576a99a6a886d0
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-11-19 16:06:00 -0300

    [:goal_net:] | create: handler - exceptions (infra)

---
+++ src/main/java/com/picpay/payment/infra/handler/error/ExceptionHandler.java
@@ -0,0 +1,14 @@
+package com.picpay.payment.infra.handler.error;
+
+import com.picpay.payment.domain.dto.error.ExceptionDTO;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class ExceptionHandler {
+
+
+
+}
