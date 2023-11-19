ed4f655e6c6ea4e159343c0aa22719ebd9e26d55
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-11-19 17:23:28 -0300

    [:sparkles:] | create: use-case - save > user (application)

--- src/main/java/com/picpay/payment/application/usecase/user/SaveUserUseCase.java
+++ src/main/java/com/picpay/payment/application/usecase/user/SaveUserUseCase.java
@@ -1,13 +1,15 @@
 package com.picpay.payment.application.usecase.user;
 
+import com.picpay.payment.domain.entities.user.User;
 import com.picpay.payment.domain.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 
 public class SaveUserUseCase {
 
+    @Autowired
     private UserRepository repository;
 
-    execute(){
-
+    public User execute(User user){
+        return this.repository.save(user);
     }
-
 }
