c8ca726adcc877367571461f7c015ffd7a6402e3
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-11-19 13:32:20 -0300

    [:sparkles:] | create: entity - user (domain)

--- src/main/java/com/picpay/payment/domain/entities/user/User.java
+++ src/main/java/com/picpay/payment/domain/entities/user/User.java
@@ -1,4 +1,30 @@
 package com.picpay.payment.domain.entities.user;
 
+import com.picpay.payment.domain.dto.user.UserDTO;
+import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.beans.BeanUtils;
+
+import java.math.BigDecimal;
+
+@Entity(name = "users") @Table(name = "users")
+@Getter @Setter
+@AllArgsConstructor @NoArgsConstructor
+@EqualsAndHashCode(of = "id")
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String firstName;
+    private String lastName;
+    @Column(unique = true)
+    private String document;
+    @Column(unique = true)
+    private String email;
+    private String password;
+    private BigDecimal balance;
+    @Enumerated(EnumType.STRING)
+    private UserType userType;
+
 }
