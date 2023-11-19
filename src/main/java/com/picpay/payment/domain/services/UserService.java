ddd00f1ebb03c725a911db9d06a664a5351aef04
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-11-19 17:21:48 -0300

    [:sparkles:] | create: service - user (domain)

--- src/main/java/com/picpay/payment/domain/services/UserService.java
+++ src/main/java/com/picpay/payment/domain/services/UserService.java
@@ -1,8 +1,20 @@
 package com.picpay.payment.domain.services;
 
+import com.picpay.payment.domain.dto.user.UserDTO;
+import com.picpay.payment.domain.entities.user.User;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public interface UserService {
+
+    public User findUserById(Long id) throws Exception;
+
+    public User createUser(UserDTO data);
+
+    public List<User> getAllUsers();
+
+    public void saveUser(User user);
 
 }
