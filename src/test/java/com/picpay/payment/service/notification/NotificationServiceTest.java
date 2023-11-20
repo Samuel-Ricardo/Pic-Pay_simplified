a097bac643e30a90179c352b3e92937fdee46309
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-11-20 13:07:11 -0300

    [:white_check_mark:] | test: service - notification (test/application)

--- src/test/java/com/picpay/payment/service/notification/NotificationServiceTest.java
+++ src/test/java/com/picpay/payment/service/notification/NotificationServiceTest.java
@@ -1,10 +1,16 @@
 package com.picpay.payment.service.notification;
 
 import com.picpay.payment.application.usecase.notification.SendNotificationUseCase;
+import static com.picpay.payment.data.NotificationData.*;
+import com.picpay.payment.data.UserData;
+import com.picpay.payment.domain.dto.notification.NotificationDTO;
 import com.picpay.payment.domain.services.NotificationService;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import static org.mockito.Mockito.*;
 import org.mockito.MockitoAnnotations;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -26,6 +32,11 @@ public class NotificationServiceTest {
         MockitoAnnotations.initMocks(this);
     }
 
-    
+    @Test
+    @DisplayName("[UNIT] | Should: send => [NOTIFICATION]")
+    void sendNotification() throws Exception {
+        service.sendNotification(UserData.VALID_COMON_USER, "Message");
+        verify(send, times(1)).execute(VALID_NOTIFICATION_DATA);
+    }
 
 }
