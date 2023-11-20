caa812f212d19d5d16650e8a7361d4dbb7df7d09
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-11-20 15:49:10 -0300

    [:white_check_mark:] | test: service - transaction > validate (test/application)

--- src/test/java/com/picpay/payment/service/transaction/TransactionServiceTest.java
+++ src/test/java/com/picpay/payment/service/transaction/TransactionServiceTest.java
@@ -3,18 +3,28 @@ package com.picpay.payment.service.transaction;
 import com.picpay.payment.application.service.TransactionServiceImpl;
 import com.picpay.payment.application.usecase.transaction.SaveTransactionUseCase;
 import com.picpay.payment.application.usecase.transaction.TransactionUseCase;
+import static com.picpay.payment.data.UserData.*;
+
+import static com.picpay.payment.data.TransactionData.*;
 import com.picpay.payment.domain.policy.transaction.CannotTransactWithoutSufficientBalancePolicy;
 import com.picpay.payment.domain.policy.transaction.MerchantUserCantTransactPolicy;
 import com.picpay.payment.domain.policy.transaction.SendNotificationOnTransactPolicy;
 import com.picpay.payment.domain.services.AuthorizationService;
 import com.picpay.payment.domain.services.UserService;
+import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import static org.mockito.MockitoAnnotations.openMocks;
+
+import static org.mockito.Mockito.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
+
+import java.math.BigDecimal;
 
 @ActiveProfiles("test")
 @SpringBootTest
@@ -39,6 +49,30 @@ public class TransactionServiceTest {
         openMocks(this);
     }
 
-    
+    @Test
+    @DisplayName("[UNIT] | Should: validate => [TRANSACTIONS]")
+    void validate() throws Exception {
 
+        var value = new BigDecimal(100);
+
+        when(cannotTransactWithoutSufficientBalancePolicy.execute(VALID_TRANSACTION.getSender(), value))
+                .thenReturn(false);
+
+        when(merchantUserCantTransactPolicy.execute(VALID_TRANSACTION.getSender().getUserType()))
+                .thenReturn(false);
+
+        when(authorizationService.isTransactionAuthorized(VALID_TRANSACTION.getSender(), value))
+                .thenReturn(true);
+
+        assertTrue(service.validate(VALID_TRANSACTION.getSender(), value));
+
+        verify(cannotTransactWithoutSufficientBalancePolicy, times(1))
+                .execute(VALID_TRANSACTION.getSender(), value);
+
+        verify(merchantUserCantTransactPolicy, times(1))
+                .execute(VALID_TRANSACTION.getSender().getUserType());
+
+        verify(authorizationService, times(1))
+                .isTransactionAuthorized(VALID_TRANSACTION.getSender(), value);
+    }
 }
