ecf251184d9eb01c9da38a0758ea3d2ef953f1c9
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-11-19 22:41:53 -0300

    [:sparkles:] | create: controller - transaction (application)

---
+++ src/main/java/com/picpay/payment/application/controllers/TransactionController.java
@@ -0,0 +1,22 @@
+package com.picpay.payment.application.controllers;
+
+import com.picpay.payment.domain.dto.transaction.TransactionDTO;
+import com.picpay.payment.domain.entities.transaction.Transaction;
+import com.picpay.payment.domain.services.TransactionService;
+import org.springframework.beans.factory.annotation.Autowired;
+import static org.springframework.http.HttpStatus.CREATED;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/transactions")
+public class TransactionController {
+
+    @Autowired
+    private TransactionService service;
+
+    
+
+}
