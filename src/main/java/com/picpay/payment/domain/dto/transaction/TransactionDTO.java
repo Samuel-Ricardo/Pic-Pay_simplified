dc03401ce6dbaa003ac249d6e570c58496c4ba0c
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-11-21 00:07:11 -0300

    [:safety_vest:] | validate: DTO - transaction (domain)

--- src/main/java/com/picpay/payment/domain/dto/transaction/TransactionDTO.java
+++ src/main/java/com/picpay/payment/domain/dto/transaction/TransactionDTO.java
@@ -1,7 +1,12 @@
 package com.picpay.payment.domain.dto.transaction;
 
+import jakarta.validation.constraints.NotBlank;
+
 import java.math.BigDecimal;
 import java.util.UUID;
 
-public record TransactionDTO(BigDecimal value, UUID senderId, UUID receiverId) {
-}
+public record TransactionDTO(
+        @NotBlank BigDecimal value,
+        @NotBlank UUID senderId,
+        @NotBlank UUID receiverId
+    ) { }
