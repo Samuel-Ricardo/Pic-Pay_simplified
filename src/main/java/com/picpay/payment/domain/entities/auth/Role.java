cf944f0ce5e4146c0ca2cd36be15c30a5f3e5dd1
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-11-21 00:10:20 -0300

    [:passport_control:] | create: entity - roles (domain/auth)

---
+++ src/main/java/com/picpay/payment/domain/entities/auth/Role.java
@@ -0,0 +1,47 @@
+package com.picpay.payment.domain.entities.auth;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import static com.picpay.payment.domain.entities.auth.Permissions.*;
+
+@Getter
+@RequiredArgsConstructor
+public enum Role {
+    USER (
+        Set.of(
+                READ_USER,
+                EXECUTE_TRANSACTION,
+                READ_TRANSACTION
+        )
+    ),
+    ADMIN(
+            Stream.concat(
+                    USER.permissions.stream(),
+                    Set.of(
+                            DELETE_USER,
+                            CREATE_USER,
+                            UPDATE_USER
+                    ).stream()
+            ).collect(Collectors.toSet())
+
+    );
+
+    private final Set<Permissions> permissions;
+
+    public List<SimpleGrantedAuthority> getAuthorities() {
+        var authorities = getPermissions()
+                .stream()
+                .map(permission -> new SimpleGrantedAuthority(permission.getPermission()))
+                .toList();
+
+        authorities.add(new SimpleGrantedAuthority("ROLE_"+this.name()));
+        return authorities;
+    }
+}
