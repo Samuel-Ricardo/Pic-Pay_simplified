1afb86a6e8a832de75023eae39e9505a04451195
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-11-21 16:47:51 -0300

    [:passport_control:] | update: service - token (domain/auth)

--- src/main/java/com/picpay/payment/domain/services/auth/token/TokenService.java
+++ src/main/java/com/picpay/payment/domain/services/auth/token/TokenService.java
@@ -1,11 +1,13 @@
 package com.picpay.payment.domain.services.auth.token;
 
 import com.picpay.payment.domain.entities.user.User;
+import jakarta.servlet.http.HttpServletRequest;
 
 import java.util.Optional;
 
 public interface TokenService {
     String generateFor(User user);
-    String validate(String token);
+    Optional<String> validate(String token);
     Optional<String> getDataFrom(String token);
+    Optional<String> getFromHeader(HttpServletRequest request);
 }
