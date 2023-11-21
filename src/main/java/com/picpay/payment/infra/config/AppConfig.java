271409340b56e3cbc84d51c94e2f7380bf58b704
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-11-21 12:30:45 -0300

    [:gear:] | create: config - app > auditorAware (infra/auth)

--- src/main/java/com/picpay/payment/infra/config/AppConfig.java
+++ src/main/java/com/picpay/payment/infra/config/AppConfig.java
@@ -1,20 +1,22 @@
 package com.picpay.payment.infra.config;
 
 import com.picpay.payment.domain.repositories.UserRepository;
+import com.picpay.payment.infra.security.auditing.ApplicationAuditAware;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.domain.AuditorAware;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.UUID;
 
 @Configuration
 @RequiredArgsConstructor
@@ -53,4 +55,8 @@ public class AppConfig {
         return config.getAuthenticationManager();
     }
 
+    @Bean
+    public AuditorAware<UUID> auditorAware() {
+        return new ApplicationAuditAware();
+    }
 }
