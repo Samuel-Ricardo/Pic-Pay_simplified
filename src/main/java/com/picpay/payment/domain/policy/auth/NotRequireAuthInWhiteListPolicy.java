02d785f51d763190a670f89030a8dd39c8342c62
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-11-21 14:46:57 -0300

    [:scroll:] | update: policy - auth > white list (domain)

--- src/main/java/com/picpay/payment/domain/policy/auth/NotRequireAuthInWhiteListPolicy.java
+++ src/main/java/com/picpay/payment/domain/policy/auth/NotRequireAuthInWhiteListPolicy.java
@@ -1,8 +1,18 @@
 package com.picpay.payment.domain.policy.auth;
 
 import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.NonNull;
+
+import java.io.IOException;
+import java.util.Optional;
 
 public interface NotRequireAuthInWhiteListPolicy {
-    void execute(FilterChain filterChain, HttpServletRequest request);
+    boolean execute(
+            FilterChain filterChain,
+            HttpServletRequest request,
+            HttpServletResponse response
+    ) throws ServletException, IOException;
 }
