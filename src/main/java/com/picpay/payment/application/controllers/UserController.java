92b2b7139ca3bafd5f328d7b738a128fd592879c
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-11-21 00:08:03 -0300

    [:safety_vest:] | validate: controller - user (domain)

--- src/main/java/com/picpay/payment/application/controllers/UserController.java
+++ src/main/java/com/picpay/payment/application/controllers/UserController.java
@@ -3,6 +3,7 @@ package com.picpay.payment.application.controllers;
 import com.picpay.payment.domain.dto.user.UserDTO;
 import com.picpay.payment.domain.entities.user.User;
 import com.picpay.payment.domain.services.UserService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import static org.springframework.http.HttpStatus.*;
 import org.springframework.http.ResponseEntity;
@@ -18,7 +19,7 @@ public class UserController  {
     private UserService service;
 
     @PostMapping
-    public ResponseEntity<User> create(@RequestBody UserDTO DTO) {
+    public ResponseEntity<User> create(@RequestBody @Valid UserDTO DTO) {
         return new ResponseEntity<>(service.saveUser(DTO), CREATED);
     }
 
