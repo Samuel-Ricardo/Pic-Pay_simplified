b49f06c76e78af2f15cebecaaad0fc353e22247c
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-11-21 16:49:08 -0300

    [:goal_net:] | catch: erro - JWT > validation (application/use-case/auth)

--- src/main/java/com/picpay/payment/application/usecase/auth/jwt/ValidateJWTUseCase.java
+++ src/main/java/com/picpay/payment/application/usecase/auth/jwt/ValidateJWTUseCase.java
@@ -2,8 +2,11 @@ package com.picpay.payment.application.usecase.auth.jwt;
 
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTVerificationException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+
+import java.util.Optional;
 
 @Component
 public class ValidateJWTUseCase {
@@ -11,11 +14,17 @@ public class ValidateJWTUseCase {
     @Autowired
     private Algorithm ALGORITHM;
 
-    public String execute(String token) {
-        return JWT.require(ALGORITHM)
-                .withIssuer("auth-api")
-                .build()
-                .verify(token)
-                .getSubject();
+    public Optional<String> execute(String token) {
+        try {
+            return Optional.of(
+                    JWT.require(ALGORITHM)
+                            .withIssuer("auth-api")
+                            .build()
+                            .verify(token)
+                            .getSubject()
+            );
+        } catch (JWTVerificationException e) {
+            return Optional.empty();
+        }
     }
 }
