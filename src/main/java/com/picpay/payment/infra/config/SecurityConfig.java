502938221aecee0d0e89c748e4d1368966457fe1
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-11-21 12:39:26 -0300

    [:gear:] | setup: config - security (infra)

--- src/main/java/com/picpay/payment/infra/config/SecurityConfig.java
+++ src/main/java/com/picpay/payment/infra/config/SecurityConfig.java
@@ -2,8 +2,16 @@ package com.picpay.payment.infra.config;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import static org.springframework.security.config.http.SessionCreationPolicy.*;
+
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.logout.LogoutHandler;
 
 @Configuration
 @EnableWebSecurity
@@ -24,5 +32,28 @@ public class SecurityConfig {
             "/webjars/**",
             "/swagger-ui.html"
     };
+
+    private final AuthenticationProvider authenticationProvider;
+    private final LogoutHandler logoutHandler;
+
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.
+                csrf(AbstractHttpConfigurer::disable).
+                authorizeHttpRequests(req ->
+                        req.requestMatchers(WHITE_LIST)
+                                .permitAll()
+                                .anyRequest()
+                                .authenticated()
+                ).
+                sessionManagement(session -> session.sessionCreationPolicy(STATELESS) ).
+                authenticationProvider(authenticationProvider).
+                logout(logout -> logout.
+                        logoutUrl("/auth/logout").
+                        addLogoutHandler(logoutHandler).
+                        logoutSuccessHandler(((request, response, authentication) -> SecurityContextHolder.clearContext()))
+                )
+        ;
+        return http.build();
+    }
 
 }
