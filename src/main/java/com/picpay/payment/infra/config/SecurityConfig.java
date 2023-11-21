7447bfabb73ed9bd52e17a9741de623d485e1cd4
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-11-21 12:31:03 -0300

    [:gear:] | create: config - security (infra)

---
+++ src/main/java/com/picpay/payment/infra/config/SecurityConfig.java
@@ -0,0 +1,16 @@
+package com.picpay.payment.infra.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+@EnableMethodSecurity
+public class SecurityConfig {
+
+
+
+}
