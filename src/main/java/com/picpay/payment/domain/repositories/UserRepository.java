760f8283626614ecd1b7147f25da8683cf97657f
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-11-21 10:21:38 -0300

    [:card_file_box:] | update: repository - user > findByEmail (domain)

--- src/main/java/com/picpay/payment/domain/repositories/UserRepository.java
+++ src/main/java/com/picpay/payment/domain/repositories/UserRepository.java
@@ -9,4 +9,5 @@ import java.util.UUID;
 public interface UserRepository extends JpaRepository<User, UUID> {
     Optional<User> findUserByDocument(String document);
     Optional<User> findUserById(UUID id);
+    Optional<User> findByEmail(String email);
 }
