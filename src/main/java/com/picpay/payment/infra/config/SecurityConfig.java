e0039b2e7eb75082a74d2b61d8a005ff4c330cd3
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-11-21 12:31:23 -0300

    [:gear:] | create: config - security > WHITE_LIST (infra)

--- src/main/java/com/picpay/payment/infra/config/SecurityConfig.java
+++ src/main/java/com/picpay/payment/infra/config/SecurityConfig.java
@@ -11,6 +11,18 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 @EnableMethodSecurity
 public class SecurityConfig {
 
-
+    private static final String[] WHITE_LIST = {
+            "/api/v1/auth/**",
+            "/v2/api-docs",
+            "/v3/api-docs",
+            "/v3/api-docs/**",
+            "/swagger-resources",
+            "/swagger-resources/**",
+            "/configuration/ui",
+            "/configuration/security",
+            "/swagger-ui/**",
+            "/webjars/**",
+            "/swagger-ui.html"
+    };
 
 }
