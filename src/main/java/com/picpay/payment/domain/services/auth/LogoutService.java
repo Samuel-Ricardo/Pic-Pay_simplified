fb71e00e05ee710af0491bb0e8a3385e9fd1a369
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-11-21 21:56:56 -0300

    [:passport_control:] | create: service - logout (domain/auth)

---
+++ src/main/java/com/picpay/payment/domain/services/auth/LogoutService.java
@@ -0,0 +1,5 @@
+package com.picpay.payment.domain.services.auth;
+
+import org.springframework.security.web.authentication.logout.LogoutHandler;
+
+public interface LogoutService extends LogoutHandler {}
