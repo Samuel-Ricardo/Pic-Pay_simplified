f516ec19ea2b1fdb72b1b39d5e14d75bd9d6118e
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-11-22 09:58:05 -0300

    [:traffic_light:] | create: route - get [all] > transactions (application/auth)

--- src/main/java/com/picpay/payment/application/controllers/TransactionController.java
+++ src/main/java/com/picpay/payment/application/controllers/TransactionController.java
@@ -7,10 +7,9 @@ import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import static org.springframework.http.HttpStatus.CREATED;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/transactions")
@@ -24,5 +23,10 @@ public class TransactionController {
         return ResponseEntity
                 .status(CREATED)
                 .body(service.transact(transaction));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Transaction>> getAll() {
+        return ResponseEntity.ok(service.findAll());
     }
 }
