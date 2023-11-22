a2dbb1685c671cdc7db8422210ff08cac84acc00
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-11-22 09:54:58 -0300

    [:sparkles:] | update: service - transaction (domain)

--- src/main/java/com/picpay/payment/domain/services/TransactionService.java
+++ src/main/java/com/picpay/payment/domain/services/TransactionService.java
@@ -5,9 +5,12 @@ import com.picpay.payment.domain.entities.transaction.Transaction;
 import com.picpay.payment.domain.entities.user.User;
 
 import java.math.BigDecimal;
+import java.util.List;
 
 public interface TransactionService {
     boolean validate(User sender, BigDecimal amount) throws Exception;
 
     Transaction transact(TransactionDTO dto) throws Exception;
+
+    List<Transaction> findAll();
 }
