5ed44088f5bd9ae6112a2db2ee29346f8cc2359d
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-11-22 14:15:46 -0300

    [:memo:] | setup: config - swagger (infra)

--- src/main/java/com/picpay/payment/infra/config/docs/SwaggerConfig.java
+++ src/main/java/com/picpay/payment/infra/config/docs/SwaggerConfig.java
@@ -1,4 +1,24 @@
 package com.picpay.payment.infra.config.docs;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+// Swagger is not compatible with Spring Boot 3 because it uses javax instead of Jakarta //
+
+//@Configuration
+//@EnableSwagger2
 public class SwaggerConfig {
+//    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build();
+    }
 }
