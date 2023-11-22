c4ea04f107913c2ec41e97a88c5cb0d097dba3d9
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-11-22 14:18:10 -0300

    [:safety_vest:] | update: validation - DTO > notification (domain)

--- src/main/java/com/picpay/payment/domain/dto/auth/LoginDTO.java
+++ src/main/java/com/picpay/payment/domain/dto/auth/LoginDTO.java
@@ -1,8 +1,13 @@
 package com.picpay.payment.domain.dto.auth;
 
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
 
 public record LoginDTO(
-        @NotBlank String email,
-        @NotBlank String password
+        @NotBlank
+        @Email String email,
+        @NotBlank
+        @Size(min = 3) String password
 ) {}
