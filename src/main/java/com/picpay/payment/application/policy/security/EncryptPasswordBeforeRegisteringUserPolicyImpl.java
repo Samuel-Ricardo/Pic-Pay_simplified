11467e4021fbcf6cf7bf7a08e1412cfa0bf010da
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-11-21 23:01:36 -0300

    [:scroll:] | implements: policy - encrypt > password (application/security)

--- src/main/java/com/picpay/payment/application/policy/security/EncryptPasswordBeforeRegisteringUserPolicyImpl.java
+++ src/main/java/com/picpay/payment/application/policy/security/EncryptPasswordBeforeRegisteringUserPolicyImpl.java
@@ -1,14 +1,20 @@
 package com.picpay.payment.application.policy.security;
 
 import com.picpay.payment.domain.policy.security.EncryptPasswordBeforeRegisteringUserPolicy;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 
 import java.util.function.Consumer;
 
 @Component
 public class EncryptPasswordBeforeRegisteringUserPolicyImpl implements EncryptPasswordBeforeRegisteringUserPolicy {
+
+    @Autowired
+    private PasswordEncoder encoder;
+
     @Override
     public void execute(String password, Consumer<String> save) {
-        
+        save.accept(encoder.encode(password));
     }
 }
