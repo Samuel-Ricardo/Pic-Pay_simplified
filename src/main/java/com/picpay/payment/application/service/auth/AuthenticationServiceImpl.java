6cdb2985f4fe70eb2b2bec77cae539ba6e5e31a8
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-11-21 22:50:21 -0300

    [:passport_control:] | implements: service - authentication > login (application/auth)

--- src/main/java/com/picpay/payment/application/service/auth/AuthenticationServiceImpl.java
+++ src/main/java/com/picpay/payment/application/service/auth/AuthenticationServiceImpl.java
@@ -1,18 +1,31 @@
 package com.picpay.payment.application.service.auth;
 
+import com.picpay.payment.application.usecase.auth.AuthenticateUserUseCase;
 import com.picpay.payment.domain.dto.auth.LoginDTO;
 import com.picpay.payment.domain.dto.auth.LoginResponseDTO;
 import com.picpay.payment.domain.dto.user.UserDTO;
 import com.picpay.payment.domain.entities.user.User;
+import com.picpay.payment.domain.services.UserService;
 import com.picpay.payment.domain.services.auth.AuthenticationService;
+import com.picpay.payment.domain.services.auth.token.TokenService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class AuthenticationServiceImpl implements AuthenticationService {
+
+    private final TokenService tokenService;
+    private final UserService userService;
+
+    private final AuthenticateUserUseCase authenticate;
+
 
     @Override
     public LoginResponseDTO login(LoginDTO DTO) {
-        return null;
+        var auth = authenticate.execute(DTO);
+        var token = tokenService.generateFor((User) auth.getPrincipal());
+        return new LoginResponseDTO(token);
     }
 
     @Override
