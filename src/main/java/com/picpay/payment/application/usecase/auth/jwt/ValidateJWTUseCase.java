8aedf2e7a9e022b1ee1d096bb68a14b2bf84f329
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-11-22 09:55:33 -0300

    [:passport_control:] | update: use-case - validate > JWT (application/auth)

--- src/main/java/com/picpay/payment/application/usecase/auth/jwt/ValidateJWTUseCase.java
+++ src/main/java/com/picpay/payment/application/usecase/auth/jwt/ValidateJWTUseCase.java
@@ -4,11 +4,13 @@ import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTVerificationException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Component;
 
 import java.util.Optional;
 
 @Component
+@Lazy
 public class ValidateJWTUseCase {
 
     @Autowired
