356c609774115c3df9b0dc44848579f7197b0d2a
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-11-21 21:07:34 -0300

    [:passport_control:] | update: use-case - get from [header] > token (application/auth)

--- src/main/java/com/picpay/payment/application/usecase/auth/jwt/GetTokenFromHeaderUseCase.java
+++ src/main/java/com/picpay/payment/application/usecase/auth/jwt/GetTokenFromHeaderUseCase.java
@@ -4,12 +4,13 @@ import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Component;
 
 import java.util.Optional;
+import java.util.function.Function;
 
 @Component
 public class GetTokenFromHeaderUseCase {
-    public Optional<String> execute(HttpServletRequest request){
-        var token = request.getHeader("Authorization");
+    public Optional<String> execute(Function<String, String> header){
 
+        var token = header.apply("Authorization");
         return token == null? Optional.empty()
                 : Optional.of(token.replace("Bearer ", ""));
     }
