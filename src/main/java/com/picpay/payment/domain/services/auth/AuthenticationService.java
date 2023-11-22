120f5ec6233a5b8cc049bb48f7cac6594f3e51e4
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-11-21 22:36:16 -0300

    [:passport_control:] | create: service - authentication (domain/auth)

--- src/main/java/com/picpay/payment/domain/services/auth/AuthenticationService.java
+++ src/main/java/com/picpay/payment/domain/services/auth/AuthenticationService.java
@@ -1,5 +1,11 @@
 package com.picpay.payment.domain.services.auth;
 
+import com.picpay.payment.domain.dto.auth.LoginDTO;
+import com.picpay.payment.domain.dto.auth.LoginResponseDTO;
+import com.picpay.payment.domain.dto.user.UserDTO;
+import com.picpay.payment.domain.entities.user.User;
+
 public interface AuthenticationService {
-    login
+    LoginResponseDTO login(LoginDTO DTO);
+    User register(UserDTO DTO);
 }
