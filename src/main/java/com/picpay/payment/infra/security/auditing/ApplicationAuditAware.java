a4928825a7bd642384cc85d477f7e6ede84cbd7f
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-11-21 21:46:59 -0300

    [:lock:] | update: auditing (infra/security)

--- src/main/java/com/picpay/payment/infra/security/auditing/ApplicationAuditAware.java
+++ src/main/java/com/picpay/payment/infra/security/auditing/ApplicationAuditAware.java
@@ -3,20 +3,18 @@ package com.picpay.payment.infra.security.auditing;
 import com.picpay.payment.domain.entities.user.User;
 import com.picpay.payment.domain.services.auth.AuthContextService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.AuditorAware;
-import org.springframework.security.authentication.AnonymousAuthenticationToken;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 
 import java.util.Optional;
 import java.util.UUID;
 
 @Component
-@RequiredArgsConstructor
 public class ApplicationAuditAware implements AuditorAware<UUID> {
 
-    private final AuthContextService context;
+    @Autowired
+    private AuthContextService context;
 
     @Override
     public Optional<UUID> getCurrentAuditor() {
