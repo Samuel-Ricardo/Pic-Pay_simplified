d6be832c53074bb273cc0d2e0a39f43cec97ef99
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-11-21 23:13:06 -0300

    [:video_game:] | create: controller - authentication (application)

---
+++ src/main/java/com/picpay/payment/application/controllers/auth/AuthenticationController.java
@@ -0,0 +1,22 @@
+package com.picpay.payment.application.controllers.auth;
+
+import com.picpay.payment.domain.dto.auth.LoginDTO;
+import com.picpay.payment.domain.dto.auth.LoginResponseDTO;
+import com.picpay.payment.domain.services.auth.AuthenticationService;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/auth")
+public class AuthenticationController {
+
+    @Autowired
+    private AuthenticationService service;
+
+    
+
+}
