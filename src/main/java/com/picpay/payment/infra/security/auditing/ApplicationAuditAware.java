7a728ebf15854d3d79de9d2053f69e5d17b1c04a
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-11-21 21:46:02 -0300

    [:lock:] | update: auditing (infra/security)

--- src/main/java/com/picpay/payment/infra/security/auditing/ApplicationAuditAware.java
+++ src/main/java/com/picpay/payment/infra/security/auditing/ApplicationAuditAware.java
@@ -1,29 +1,25 @@
 package com.picpay.payment.infra.security.auditing;
 
 import com.picpay.payment.domain.entities.user.User;
+import com.picpay.payment.domain.services.auth.AuthContextService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.AuditorAware;
 import org.springframework.security.authentication.AnonymousAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
 
 import java.util.Optional;
 import java.util.UUID;
 
+@Component
+@RequiredArgsConstructor
 public class ApplicationAuditAware implements AuditorAware<UUID> {
+
+    private final AuthContextService context;
+
     @Override
     public Optional<UUID> getCurrentAuditor() {
-
-        Authentication authentication = SecurityContextHolder
-                .getContext()
-                .getAuthentication();
-
-        if(
-                authentication == null ||
-               !authentication.isAuthenticated() ||
-                authentication instanceof AnonymousAuthenticationToken
-        ) { return Optional.empty(); }
-
-        User user = (User) authentication.getPrincipal();
-        return Optional.ofNullable(user.getId());
+        return context.getUser().map(User::getId);
     }
 }
