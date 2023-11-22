d9d02375ca3ae1a7e5686102e4ce611679193ec1
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-11-22 14:17:37 -0300

    [:safety_vest:] | update: validation - DTO > user (domain)

--- src/main/java/com/picpay/payment/domain/dto/user/UserDTO.java
+++ src/main/java/com/picpay/payment/domain/dto/user/UserDTO.java
@@ -2,7 +2,10 @@ package com.picpay.payment.domain.dto.user;
 
 import com.picpay.payment.domain.entities.auth.Role;
 import com.picpay.payment.domain.entities.user.UserType;
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 
 import java.math.BigDecimal;
 
@@ -13,15 +16,17 @@ public record UserDTO(
         String lastName,
         @NotBlank
         String document,
-        @NotBlank
+        @NotNull
         BigDecimal balance,
         @NotBlank
+        @Email
         String email,
         @NotBlank
+        @Size(min = 3)
         String password,
-        @NotBlank
+        @NotNull
         UserType userType,
-        @NotBlank
+        @NotNull
         Role role
 ) {
 }
