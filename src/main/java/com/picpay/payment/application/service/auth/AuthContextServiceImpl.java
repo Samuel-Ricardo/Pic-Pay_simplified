a8125fe5861ba42912cc9714b55246a0f1f57cb2
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-11-21 21:35:06 -0300

    [:passport_control:] | implements: service - AuthContext > save (application/auth)

--- src/main/java/com/picpay/payment/application/service/auth/AuthContextServiceImpl.java
+++ src/main/java/com/picpay/payment/application/service/auth/AuthContextServiceImpl.java
@@ -1,7 +1,9 @@
 package com.picpay.payment.application.service.auth;
 
+import com.picpay.payment.application.usecase.auth.SaveAuthContextUseCase;
 import com.picpay.payment.domain.entities.user.User;
 import com.picpay.payment.domain.services.auth.AuthContextService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
@@ -9,16 +11,18 @@ import org.springframework.stereotype.Service;
 import java.util.Optional;
 
 @Service
+@RequiredArgsConstructor
 public class AuthContextServiceImpl implements AuthContextService {
+
+    private final SaveAuthContextUseCase save;
 
     @Override
     public void saveContext(User user) {
-        var authentication = new UsernamePasswordAuthenticationToken(user, null, user.getAuthorities());
-        SecurityContextHolder.getContext().setAuthentication(authentication);
+        save.execute(user);
     }
 
     @Override
     public Optional<User> getUser() {
-        return Optional.empty();
+        
     }
 }
