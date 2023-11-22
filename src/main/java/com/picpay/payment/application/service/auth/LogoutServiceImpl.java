962fa95a8e951ae5913036af87a911c819876145
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-11-21 21:59:12 -0300

    [:passport_control:] | setup: service - logout (application/auth)

---
+++ src/main/java/com/picpay/payment/application/service/auth/LogoutServiceImpl.java
@@ -0,0 +1,22 @@
+package com.picpay.payment.application.service.auth;
+
+import com.picpay.payment.domain.services.auth.AuthContextService;
+import com.picpay.payment.domain.services.auth.LogoutService;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Service;
+
+@Service
+public class LogoutServiceImpl implements LogoutService {
+
+    @Autowired
+    private AuthContextService context;
+
+
+    @Override
+    public void logout(HttpServletRequest request, HttpServletResponse response, Authentication authentication) {
+        
+    }
+}
