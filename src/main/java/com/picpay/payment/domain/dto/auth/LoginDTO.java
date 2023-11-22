e61ccf9d12782181e59181007111a37f0e115089
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-11-21 22:49:51 -0300

    [:passport_control:] | update: DTO - login (domain/auth)

--- src/main/java/com/picpay/payment/domain/dto/auth/LoginDTO.java
+++ src/main/java/com/picpay/payment/domain/dto/auth/LoginDTO.java
@@ -4,5 +4,5 @@ import jakarta.validation.constraints.NotBlank;
 
 public record LoginDTO(
         @NotBlank String email,
-        @NotBlank   String password
+        @NotBlank String password
 ) {}
