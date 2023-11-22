6bb43f9432b89582b76230bbcfb45a5360b2aadf
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-11-21 22:43:11 -0300

    [:passport_control:] | create: use-case - authenticate > user (application/auth)

---
+++ src/main/java/com/picpay/payment/application/usecase/auth/AuthenticateUserUseCase.java
@@ -0,0 +1,20 @@
+package com.picpay.payment.application.usecase.auth;
+
+import com.picpay.payment.domain.dto.user.UserDTO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Component;
+
+@Component
+public class AuthenticateUserUseCase {
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    public Authentication execute(UserDTO DTO){
+        var credentials = new UsernamePasswordAuthenticationToken(DTO.email(), DTO.password());
+        return authenticationManager.authenticate(credentials);
+    }
+}
