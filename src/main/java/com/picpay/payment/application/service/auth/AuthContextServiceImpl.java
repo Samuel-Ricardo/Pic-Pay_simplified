b7c09371b86b922650e2b81fd8d6345e77c42b14
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-11-21 21:29:44 -0300

    [:passport_control:] | implements: service - AuthContext > save (application/auth)

--- src/main/java/com/picpay/payment/application/service/auth/AuthContextServiceImpl.java
+++ src/main/java/com/picpay/payment/application/service/auth/AuthContextServiceImpl.java
@@ -2,6 +2,8 @@ package com.picpay.payment.application.service.auth;
 
 import com.picpay.payment.domain.entities.user.User;
 import com.picpay.payment.domain.services.auth.AuthContextService;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
@@ -11,7 +13,8 @@ public class AuthContextServiceImpl implements AuthContextService {
 
     @Override
     public void saveContext(User user) {
-        
+        var authentication = new UsernamePasswordAuthenticationToken(user, null, user.getAuthorities());
+        SecurityContextHolder.getContext().setAuthentication(authentication);
     }
 
     @Override
