9921a2853617a131bd68c0bd263aa3e6d8ec88c1
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-11-22 15:10:26 -0300

    [:mailbox_with_mail:] | update: service - notification (application)

--- src/main/java/com/picpay/payment/application/usecase/notification/SendNotificationUseCase.java
+++ src/main/java/com/picpay/payment/application/usecase/notification/SendNotificationUseCase.java
@@ -2,10 +2,12 @@ package com.picpay.payment.application.usecase.notification;
 
 import com.picpay.payment.domain.dto.notification.NotificationDTO;
 import com.picpay.payment.domain.gateways.NotificationGateway;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class SendNotificationUseCase {
+    @Autowired
     protected NotificationGateway gateway;
 
     public void execute(NotificationDTO notification) throws Exception {
