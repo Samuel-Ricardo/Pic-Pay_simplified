d5ed8599cf5ad64e98316b5fc16b3286307c35a4
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-11-22 09:52:56 -0300

    [:gear:] | create: config - Cryptography (infra/security)

---
+++ src/main/java/com/picpay/payment/infra/config/CryptographyConfig.java
@@ -0,0 +1,22 @@
+package com.picpay.payment.infra.config;
+
+import com.auth0.jwt.algorithms.Algorithm;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.context.annotation.Primary;
+
+@Configuration
+public class CryptographyConfig {
+
+    @Value("${app.security.token.secret}")
+    private String AUHT_TOKEN_SECRET;
+
+    @Bean
+    @Primary
+    @Lazy
+    public Algorithm algorithm() {
+        return Algorithm.HMAC256(AUHT_TOKEN_SECRET);
+    }
+}
