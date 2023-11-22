aafa4007f358b4bc30df73a47f22fed9931bb4a1
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-11-21 23:15:06 -0300

    [:traffic_light:] | create: route - login (application/auth)

--- src/main/java/com/picpay/payment/application/controllers/auth/AuthenticationController.java
+++ src/main/java/com/picpay/payment/application/controllers/auth/AuthenticationController.java
@@ -6,6 +6,7 @@ import com.picpay.payment.domain.services.auth.AuthenticationService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,6 +18,12 @@ public class AuthenticationController {
     @Autowired
     private AuthenticationService service;
 
-    
+    @PostMapping("/login")
+    public ResponseEntity<LoginResponseDTO> login(
+            @RequestBody
+            @Valid LoginDTO DTO
+        ) {
+        return ResponseEntity.ok(service.login(DTO));
+    }
 
 }
