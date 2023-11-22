3f46cfd9abf5883e79d78c13ba7c4995d61a914d
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-11-21 21:54:09 -0300

    [:gear:] | update: config - security (infra)

--- src/main/java/com/picpay/payment/infra/config/SecurityConfig.java
+++ src/main/java/com/picpay/payment/infra/config/SecurityConfig.java
@@ -1,6 +1,7 @@
 package com.picpay.payment.infra.config;
 
 import com.auth0.jwt.algorithms.Algorithm;
+import com.picpay.payment.infra.middleware.auth.jwt.JWTAuthMiddleware;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
@@ -15,6 +16,7 @@ import static org.springframework.security.config.http.SessionCreationPolicy.*;
 
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.authentication.logout.LogoutHandler;
 
 @Configuration
@@ -42,6 +44,7 @@ public class SecurityConfig {
 
     private final AuthenticationProvider authenticationProvider;
     private final LogoutHandler logoutHandler;
+    private final JWTAuthMiddleware JWT_MIDDLEWARE;
 
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.
@@ -54,6 +57,7 @@ public class SecurityConfig {
                 ).
                 sessionManagement(session -> session.sessionCreationPolicy(STATELESS) ).
                 authenticationProvider(authenticationProvider).
+                addFilterBefore(JWT_MIDDLEWARE, UsernamePasswordAuthenticationFilter.class).
                 logout(logout -> logout.
                         logoutUrl("/auth/logout").
                         addLogoutHandler(logoutHandler).
